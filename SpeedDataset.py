4a469319d14b67da823af4df6e65c5a5791572bf
Andrew Pynch <andrewpynchbusiness@gmail.com>
2020-05-14 12:16:52 -0700

    Add speeddataset.py file'

---
+++ SpeedDataset.py
@@ -0,0 +1,48 @@
+from __future__ import print_function, division
+import os
+import re
+
+import torch
+import pandas as pd
+from skimage import io, transform
+import numpy as np
+import matplotlib.pyplot as plt
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+
+class SpeedDataset(Dataset):
+    def __init__(self, root_dir, transform=None):
+        '''
+        Args:
+            root_dir: Root directory containing images with labels in filename
+            transform (callable, optional): Optional transforms to be applied to to a sample
+        '''
+        self.root_dir = root_dir
+        self.items = os.listdir(self.root_dir)
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.items)
+
+    def __getitem__(self, idx):
+        img_name = os.path.join(self.root_dir,
+                                self.items[idx])
+
+        image = io.imread(img_name)
+        regex = re.split('/|_', img_name)
+        label = float(regex[1])
+        sample = {'image': image, 'label': label}
+
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample
+
+'''
+data = SpeedDataset(root_dir='vidCaps/')
+
+sample = data[0]
+print(sample['label'], 'MPH')
+plt.imshow(sample['image'])
+```
+
